ecab5dec7347b2aed993f69115fa948191cf24cb
binLep <45154147+binLep@users.noreply.github.com>
2022-05-13 00:00:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -44,9 +44,9 @@ def args():
     parser.add_argument('-k', '--gtk', dest='gtk', type=str,
                         default='', help='Github Token，若非空值则使用 Github Actions 发送播报邮件')
     parser.add_argument('-ms', '--mail_smtp', dest='mail_smtp',
-                        type=str, default='smtp.qq.com', help='用于发送播报信息的邮箱 SMTP 服务器')
+                        type=str, default='', help='用于发送播报信息的邮箱 SMTP 服务器')
     parser.add_argument('-mu', '--mail_user', dest='mail_user', type=str,
-                        default='threatbroadcast@qq.com', help='用于发送播报信息的邮箱账号')
+                        default='', help='用于发送播报信息的邮箱账号')
     parser.add_argument('-mp', '--mail_pass', dest='mail_pass',
                         type=str, default='', help='用于发送播报信息的邮箱密码')
     parser.add_argument('-qu', '--qq_user', dest='qq_user',
@@ -95,6 +95,7 @@ def main(top, auto_commit, gtk, mail_smtp, mail_user, mail_pass, qq_user, qq_pas
 
     if all_cves:
         page.to_page(top)
+        print(mail_user)
         mail.to_mail(gtk, all_cves, mail_smtp, mail_user, mail_pass)
         qq.to_group(all_cves, qq_user, qq_pass)
         wechat.to_wechat(all_cves)
